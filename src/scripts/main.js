cd85e69212d5ceeba497439161bcb93e7e1d79c2
Illia <hetsko.ilya@gmail.com>
2025-07-12 01:25:34 +0200

    initial website version complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function toggleScrollLock() {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('scroll-lock');
+  } else {
+    document.body.classList.remove('scroll-lock');
+  }
+}
+
+window.addEventListener('hashchange', toggleScrollLock);
+
+toggleScrollLock();
